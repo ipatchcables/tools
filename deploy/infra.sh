d2e93e1d9dd28f2046b48294beb4149813b13991
ipatchcables <30351225+ipatchcables@users.noreply.github.com>
2021-02-08 15:42:28 -0500

    Create infra.sh

---
+++ deploy/infra.sh
@@ -0,0 +1,7 @@
+mkdir /root/cs && mkidr /root/cs/opt
+wget -O /root/cs/go1.15.8.linux-amd64.tar.gz https://golang.org/dl/go1.15.8.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> /root/.bashrc
+
+git clone https://github.com/michenriksen/aquatone.git /root/cs/opt/aqua
+git clone https://github.com/OJ/gobuster.git /root/cs/opt/gobuster
+git clone https://github.com/jaeles-project/gospider /root/opt/gospider
